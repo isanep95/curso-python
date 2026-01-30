8e5b70ecb6f8a863b6820d60f5b73a935810ef8a
Isabela <iclln.4@gmail.com>
2026-01-30 17:16:16 -0300

    Adiciona operadores.py

---
+++ operadores.py
@@ -0,0 +1,9 @@
+# Operadores Matemáticos
+
+# Operações básicas: +, -, *, /, %, **
+# % -> resto da divisão! Ex.: 4 % 2 = 0 (4 dividido por 2 tem resto 0)
+# ** -> elevado a, 4 elevado a 2 (potência)
+
+#Operadores de comparação: ==, !=, >, <, >=, <=
+
+print(2+2)
