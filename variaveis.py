90814fc598dfcd2749d79b060bab9cfd1035d32c
Isabela <iclln.4@gmail.com>
2026-01-30 16:57:46 -0300

    Atualiza hello.py e adiciona variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,10 @@
+# Variáveis e tipos de dados "básicos"
+
+# Uma variável é um espaço na memória onde armazenamos um valor.
+
+nome = "Isabela"  # variável do tipo string (texto)
+idade = 30       # variável do tipo inteiro (número sem casa decimal)
+altura = 1.62    # variável do tipo float (número com casa decimal)
+estudando = True  # variável do tipo booleano (True ou False)
+
+print(f"Olá, meu nome é {nome}, tenho {idade} anos, minha altura é {altura}m.")
