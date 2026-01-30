d21e932f81649bed966855c2a7e0210efa14db3c
Isabela <iclln.4@gmail.com>
2026-01-30 17:08:41 -0300

    Atualiza variaveis.py

--- variaveis.py
+++ variaveis.py
@@ -7,4 +7,10 @@ idade = 30       # variável do tipo inteiro (número sem casa decimal)
 altura = 1.62    # variável do tipo float (número com casa decimal)
 estudando = True  # variável do tipo booleano (True ou False)
 
-print(f"Olá, meu nome é {nome}, tenho {idade} anos, minha altura é {altura}m.")
+# print(f"Olá, meu nome é {nome}, tenho {idade} anos, minha altura é {altura}m.")
+
+nome = input("Digite seu nome: ") # entrada de texto
+idade = int(input("Digite sua idade: ")) # entrada de número inteiro
+altura = float(input("Digite sua altura em metros: ")) # entrada de número convertida pra float
+
+
