4e7788b7d6ac2650e736bb93d070a38c9eeffdda
Isabela <iclln.4@gmail.com>
2026-02-02 14:51:11 -0300

    Atualiza 4condições.py

---
+++ 4condições.py
@@ -0,0 +1,14 @@
+#Condicionais
+
+#São estruturas que permitem ao nosso programa tomar decisões com base
+#em condições específicas. Em outras palavras, o programa pode executar
+#ações diferentes dependendo de uma situação específica.
+
+#Exemplo:
+
+#Você está em uma cafeteria e está com pouca grana.
+#O cappuccino custa 10 reais, café com leite 7 e o café simples 4.
+
+#Se você tiver 10 reais ou mais, pode comprar o cappuccino.
+#Se tiver 7 reais ou mais, pode comprar o café com leite.
+#Se não, pode comprar o café simples.
