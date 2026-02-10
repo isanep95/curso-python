c4c20829d3e6d62eaf664aa2cb11ae4ac47a648d
Isabela <iclln.4@gmail.com>
2026-02-10 15:46:53 -0300

    Adicionar 7uniaoconceitos.py

---
+++ 7uniaoconceitos.py
@@ -0,0 +1,21 @@
+# União de Conceitos
+# Todas as estruturas podem ser usadas separadamente, mas
+# em um programa "real", vamos unindo todas essas estruturas 
+# para criarmos os cenários que precisamos para resolver um problema!
+
+# Exemplo: 
+# Você quer saber se uma palavra contém a letra Y
+
+#palavra = "Python"
+
+#for letra in palavra:
+#    if letra == "y":
+#        print("A palavra contém a letra Y")
+
+palavra = "isabela"
+letraprocurada = "i"
+
+for letra in palavra:
+    if letra == letraprocurada:
+        print(f"A palavra contém a letra {letraprocurada}")
+        
